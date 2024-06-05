95a373e6c71c6b030366790136762f158e15a76a
taki0786 <119153138+taki0786@users.noreply.github.com>
2024-06-05 22:11:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -65,8 +65,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" el
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌ Don't send me messages directly I'm only File Share bot !
-👨‍💻 Bot Devloper @Orewa_Zoro_San < >"
+USER_REPLY_TEXT = "❌ Don't send me messages directly I'm only File Share bot !👨‍💻 Bot Devloper @Orewa_Zoro_San < >"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(6763245675)
