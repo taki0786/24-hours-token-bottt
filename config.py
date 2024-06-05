f580181d6fd04b60acbd7936954d2048c04ad78a
taki0786 <119153138+taki0786@users.noreply.github.com>
2024-06-05 21:41:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -66,7 +66,7 @@ DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌ Don't send me messages directly I'm only File Share bot !
-👨‍💻 Bot Devloper @Orewa_Zoro_San< >"
+👨‍💻 Bot Devloper @Orewa_Zoro_San < >"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(6763245675)
